1a12167fd8c5fc3f3bcf6d76f9ab040f508f05ef
fosferon <145221245+fosferon@users.noreply.github.com>
2026-04-18 22:42:19 +0300

    chore: bump elixir requirement to ~> 1.19

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule LlmCore.MixProject do
     [
       app: :llm_core,
       version: @version,
-      elixir: "~> 1.17",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
